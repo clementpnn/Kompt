fa2aa72bbc3463a8d03bd6370cc6d8e9fb106a1d
Ouallet-Sandie <ouallet.sandie@outlook.fr>
2023-01-11 11:00:23 +0100

    router

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,19 +1,29 @@
 import { useForm, SubmitHandler } from 'react-hook-form'
-// import { FormValues } from './interfaces/interfaces'
 import { Routes, Route, Outlet, Link } from "react-router-dom";
 
 import Login from './routes/login'
 import Register from './routes/register'
+import Layout from './components/navbar'
+import Coucou from './components/test'
+
 
 export default function App() {
   return (
+    <div>
 
-  <Routes>
+      <Routes>
 
-    <Route path="/" element={<Login />} />
-    <Route path="/login" element={<Login />} />
-    <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
 
-  </Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/test" element={<Coucou />} />
+        </Route>
+
+      </Routes>
+
+    </div>
   )
 }
+
+
--- webapp/src/routes/login.tsx
+++ webapp/src/routes/login.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useForm, SubmitHandler } from 'react-hook-form'
 import { FormValues } from '../interfaces/interfaces'
 
