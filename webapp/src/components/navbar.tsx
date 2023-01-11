25a3d1d71dae121b7f88bc9306c425bda25757e8
Ouallet-Sandie <ouallet.sandie@outlook.fr>
2023-01-11 19:04:13 +0100

    navbar + badges

--- webapp/src/components/navbar.tsx
+++ webapp/src/components/navbar.tsx
@@ -1,27 +1,60 @@
 import { Routes, Route, Outlet, Link } from "react-router-dom";
 
-const Navbar = () => {
-    return (
-        <div>
-        {/* A "layout route" is a good place to put markup you want to
-            share across all the pages on your site, like navigation. */}
-            <nav>
-                <div>
-                    <Link to="/">Kompt</Link>
-                </div>
-                <div>
-                    <button>Join Group</button>
-                    <button>Create Group</button>
-                </div>
-                {/* <div>
-                    <p>Profil</p>
-                </div> */}
 
-            </nav>
-    
-            <Outlet />
+function NavbarLog() {
+    return (
+        <div className="px-20 border-b w-full fixed bg-white z-10">
+        
+            <div className="navbar bg-base-100 p-0">
+                <div className="flex-1">
+                    <Link to="/" className="btn btn-ghost normal-case text-xl">Kompt</Link>
+                </div>
+                <div className="flex-none">
+                    <button className="btn btn-ghost">Join Group</button>
+                    <button className="btn btn-ghost">Create Group</button>
+                </div>
+            </div>
+            
         </div>
-    );
+    )
 }
 
-export default Navbar
+function NavbarNotLog() {
+    return (
+        <div className="px-20 py-2.5 border-b w-full fixed">
+        
+            <div className="navbar bg-base-100 p-0">
+                <div className="flex-1">
+                    <Link to="/" className="btn btn-ghost normal-case text-xl">Kompt</Link>
+                </div>
+                <div className="flex-none">
+                    <button className="btn btn-ghost">Log In</button>
+                    <button className="btn btn-ghost">Sign In</button>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+export default function Navbar() {
+    
+    const isLogged = true;
+    return (
+        <>
+            {isLogged ? (
+                <NavbarLog />
+            ) : (
+                <NavbarNotLog />
+            )}
+            <Outlet />
+        </>
+    );
+}
