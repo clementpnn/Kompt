d92be8b476e240873ddb0e9023cdcc7d7c07d482
Victor Huang <huang.victor.mail@gmail.com>
2023-01-12 15:34:13 +0100

    correction page

--- webapp/src/routes/landingPage.tsx
+++ webapp/src/routes/landingPage.tsx
@@ -1,13 +1,14 @@
 
 import Table from "../components/table/table"
 import HeaderGroup from '../components/headerGroup'
+import { Outlet } from "react-router-dom"
 
 export default function Landing() {
     return (
         <div>
             <HeaderGroup groupName={"Test"} groupId={2} groupMemberNumber={4}/>
             <Table />
-            {/* <Outlet /> */}
+            <Outlet />
         </div>
     )
 }
