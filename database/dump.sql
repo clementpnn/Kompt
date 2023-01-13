99fdbb669dc13ba6ea4a695c0744e167accc9d57
Clément <95907390+clementpnn@users.noreply.github.com>
2023-01-13 15:47:54 +0100

    push DB

--- database/dump.sql
+++ database/dump.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS collocations
 (
-  id          TEXT PRIMARY KEY DEFAULT uuid-generate-v4() NOT NULL,
+  id          INT  PRIMARY KEY AUTO_INCREMENT NOT NULL,
   name        TEXT NOT NULL,
   secret_code TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS users
 (
-  id       TEXT PRIMARY KEY NOT NULL,
+  id       INT  PRIMARY KEY AUTO_INCREMENT NOT NULL,
   name     TEXT NOT NULL,
   email    TEXT NOT NULL,
   password TEXT NOT NULL
@@ -15,8 +15,8 @@ CREATE TABLE IF NOT EXISTS users
 
 CREATE TABLE IF NOT EXISTS collocation_roles
 (
-  collocation_id TEXT NOT NULL,
-  user_id        TEXT NOT NULL,
+  collocation_id INT  NOT NULL,
+  user_id        INT  NOT NULL,
   role           ENUM('member', 'admin') NOT NULL,
   PRIMARY KEY (collocation_id, user_id),
   FOREIGN KEY (collocation_id) REFERENCES collocations (id),
@@ -25,25 +25,25 @@ CREATE TABLE IF NOT EXISTS collocation_roles
 
 CREATE TABLE IF NOT EXISTS expenses
 (
-  id             TEXT   PRIMARY KEY DEFAULT uuid-generate-v4() NOT NULL,
+  id             INT    PRIMARY KEY AUTO_INCREMENT NOT NULL,
   date           DATE   NOT NULL,
   description    TEXT   NOT NULL,
   amount         REAL   NOT NULL,
-  payers         TEXT[] NOT NULL,
+  payers         TEXT   NOT NULL,
   payers_amount  REAL   NOT NULL,
-  user_id        TEXT   NOT NULL,
-  collocation_id TEXT   NOT NULL,
+  user_id        INT    NOT NULL,
+  collocation_id INT    NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users (id),
   FOREIGN KEY (collocation_id) REFERENCES collocations (id)
 );
 
 CREATE TABLE IF NOT EXISTS payments
 (
-  id         TEXT PRIMARY KEY DEFAULT uuid-generate-v4() NOT NULL,
+  id         INT  PRIMARY KEY AUTO_INCREMENT NOT NULL,
   date       DATE NOT NULL,
   amount     REAL NOT NULL,
-  sender_id  TEXT NOT NULL,
-  expense_id TEXT NOT NULL,
+  sender_id  INT  NOT NULL,
+  expense_id INT  NOT NULL,
   FOREIGN KEY (expense_id) REFERENCES expenses (id),
   FOREIGN KEY (sender_id) REFERENCES users (id)
 );
